905c49c697340155c34f79f4d9ec11fa39fe4464
Roman Kukharuk <roman.kukharuk@gmail.com>
2023-11-13 22:06:27 -0500

    unfollow all

--- linkedin/Unfollow all company.js
+++ linkedin/Unfollow all company.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Unfollow all.user
+// @name         Unfollow all company.user
 // @namespace    http://tampermonkey.net/
 // @version      0.0.1
 // @description  Simple way to unfollow all those pages/companies to make your feed cleaner
@@ -13,10 +13,18 @@
     'use strict';
 
     var followButtonSelector = "[class='artdeco-button__text']";
+    var followButtonXPath = '//*[@class="artdeco-button__text" and text()="Following"]';
+    var unFollowButtonXPath = '//*[@class="artdeco-button__text" and text()="Unfollow"]';
+
     var popup = '[role="alertdialog"]';
 
-    waitForElm(followButtonSelector).then((elm) => {
+    waitForElmXpath(followButtonXPath).then((elm) => {
         console.log('Follow button is found!');
+        elm.click();
+    });
+
+    waitForElmXpath(unFollowButtonXPath).then((elm) => {
+        console.log('Unfollow button is found!');
         elm.click();
     });
 
@@ -33,6 +41,33 @@ function waitForElm(selector) {
             if (document.querySelector(selector)) {
                 observer.disconnect();
                 resolve(document.querySelector(selector));
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
+
+function waitForElmXpath(selector) {
+    return new Promise(resolve => {
+        var obj = document.evaluate(
+                              selector,
+                              document,
+                              null,
+                              XPathResult.FIRST_ORDERED_NODE_TYPE,
+                              null
+                            ).singleNodeValue;
+        if (obj) {
+            return resolve(obj);
+        }
+
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                observer.disconnect();
+                resolve(obj);
             }
         });
 
