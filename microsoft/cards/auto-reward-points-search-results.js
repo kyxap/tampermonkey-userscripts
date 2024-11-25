510db20eaf1a366b3fff05f270395d593473417f
kyxap <kyxap@users.noreply.github.com>
2024-11-25 08:04:18 -0500

    New page for puzzle

--- microsoft/cards/auto-reward-points-search-results.js
+++ microsoft/cards/auto-reward-points-search-results.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Auto Microsoft Reword Points Cards 3 of 3 | Closes search results
 // @namespace    https://www.bing.com/
-// @version      0.0.7
+// @version      0.0.8
 // @description  Closes search results
 // @match        https://www.bing.com/search?form=&q=*
 // @match        https://www.bing.com/search?q=*
 // @match        https://www.bing.com/search?form=*
 // @match        https://www.bing.com/?form=*
+// @match        https://www.bing.com/spotlight/imagepuzzle*
 // @grant        none
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bing.com
 // @updateURL    https://github.com/kyxap/tampermonkey-userscripts/raw/main/microsoft/cards/auto-reward-points-search-results.js
